4c22cae817bd2843995a9f293c69afbf3419c13a
Chetan7528 <kaklotarchetan7528@gmail.com>
2026-02-18 10:47:39 +0530

    all changes done

--- android/app/src/main/java/com/fitandmeet/app/MainActivity.kt
+++ android/app/src/main/java/com/fitandmeet/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.datingapp
+package com.fitandmeet.app
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "datingapp"
+  override fun getMainComponentName(): String = "FIT AND MEET"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
